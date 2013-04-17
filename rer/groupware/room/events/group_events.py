e6884d4e7c9632ee49a891f40f3c8df56fa3867e
cekk <andrea.cecchi85@gmail.com>
2013-04-17 12:10:59 +0000

    removed event for adding in group

--- rer/groupware/room/events/group_events.py
+++ rer/groupware/room/events/group_events.py
@@ -3,44 +3,16 @@
 from Products.CMFCore.utils import getToolByName
 from zope.app.component.hooks import getSite
 
-def addNotifyGroup(info_dict,event):
-    group_id=info_dict['group'].getId()
-    user_id=info_dict['user_id']
-    if not '.' in group_id:
-        return
-    site_root=getSite()
-    acl_users=getToolByName(site_root,'acl_users')
-    room_id = group_id[:group_id.index('.')]
-    user=acl_users.getUser(user_id)
-    group_notify_news_events="%s.notifyNewsEvents" %room_id
-    group_notify_docs="%s.notifyDocs" %room_id
-    user_groups=user.getGroups()
-    already_signed=False
-    for group in user_groups:
-        if group.startswith(room_id) and not group == group_id:
-            if group in [group_notify_news_events,group_notify_docs]:
-                already_signed = True
-            else:
-                group = acl_users.getGroup(group)
-                group.removeMember(user_id)
-    if already_signed:
-        return
-    group = acl_users.getGroup(group_notify_news_events)
-    if not group:
-        return
-    group.addMember(user_id)
 
-def removeNotifyGroup(info_dict,event):
-    group_id=info_dict['group'].getId()
+def removeNotifyGroup(info_dict, event):
+    group_id = info_dict['group'].getId()
     if not '.' in group_id:
         return
     room_id = group_id[:group_id.index('.')]
-    site_root=getSite()
-    acl_users=getToolByName(site_root,'acl_users')
-    notification_groups=["%s.notifyNewsEvents" %room_id,"%s.notifyDocs" %room_id]
-    for group_id in notification_groups: 
+    site_root = getSite()
+    acl_users = getToolByName(site_root, 'acl_users')
+    notification_groups = ["%s.notifyNewsEvents" % room_id, "%s.notifyDocs" % room_id]
+    for group_id in notification_groups:
         group = acl_users.getGroup(group_id)
         if info_dict['user_id'] in group.getMemberIds():
             group.removeMember(info_dict['user_id'])
-    
-            
