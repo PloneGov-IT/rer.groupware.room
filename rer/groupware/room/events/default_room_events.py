77fc317f9b6afc5767873925281746a5d3acffb6
cekk <andrea.cecchi85@gmail.com>
2013-05-10 08:20:50 +0000

    fixed events

--- rer/groupware/room/events/default_room_events.py
+++ rer/groupware/room/events/default_room_events.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from zope.i18n import translate
-from rer.groupware.room import roomMessageFactory as _
 from plone.i18n.normalizer.interfaces import IURLNormalizer
-from zope.component._api import queryUtility
 from Products.CMFCore.utils import getToolByName
-from zope.interface import alsoProvides
+from rer.groupware.room import logger
+from rer.groupware.room import roomMessageFactory as _
 from rer.groupware.room.interfaces import IRoomArea
+from zope.component._api import queryUtility
+from zope.i18n import translate
+from zope.interface import alsoProvides
 
 
 class CreateRoomStructure(object):
@@ -94,7 +95,6 @@ class CreateRoomStructure(object):
         """
         Create a forum in the room
         """
-        room_id = self.context.getId()
         forum_id = self.context.invokeFactory(id=self.generateId(title),
                                               type_name='PloneboardForum',
                                               title=title,
@@ -122,16 +122,130 @@ class CreateRoomStructure(object):
         return id
 
 
-def createGroups(room, event):
-    """
-    This event throw some specified events for room initial setup
-    """
+class CreateGroups(object):
+
+    def __init__(self, context, event):
+        """
+        Create the groups for this room.
+        These groups will be used to manage localroles
+        """
+        self.context = context
+        groups_tool = getToolByName(self.context, 'portal_groups')
+        room_id = self.context.getId()
+        room_title = self.context.Title()
+        sgm_groups = []
+        #create members group
+        groups_tool.addGroup(id='%s.members' % room_id,
+                             title=translate(_(u"${room_title} members",
+                                               mapping={u"room_title": room_title}),
+                                             context=self.context.REQUEST))
+        sgm_groups.append('%s.members' % room_id)
+        #create members adv group
+        groups_tool.addGroup(id='%s.membersAdv' % room_id,
+                             title=translate(_(u"${room_title} membersAdv",
+                                               mapping={u"room_title": room_title}),
+                                             context=self.context.REQUEST))
+        sgm_groups.append('%s.membersAdv' % room_id)
+        #create coordinators group
+        groups_tool.addGroup(id='%s.coordinators' % room_id,
+                             title=translate(_(u"${room_title} coordinators",
+                                               mapping={u"room_title": room_title}),
+                                             context=self.context.REQUEST))
+        #create hosts group
+        groups_tool.addGroup(id='%s.hosts' % room_id,
+                             title=translate(_(u"${room_title} hosts",
+                                               mapping={u"room_title": room_title}),
+                                             context=self.context.REQUEST))
+        sgm_groups.append('%s.hosts' % room_id)
+
+        #set SGM properties to allow coordinators to handle other groups
+        self.addSGMEntries(sgm_groups, '%s.coordinators' % room_id)
+
+    def addSGMEntries(self, managed_groups, coordinator):
+        """
+        Set SGM properties with new-created groups.
+        """
+        portal_properties = getToolByName(self.context, 'portal_properties', None)
+        if not portal_properties:
+            return
+        sgm_properties = getattr(portal_properties, 'simple_groups_management_properties', None)
+        if not sgm_properties:
+            return
+        sgm_groups = set(sgm_properties.getProperty('sgm_data', None))
+        for group in managed_groups:
+            sgm_groups.add('%s|%s' % (coordinator, group))
+
+        sgm_properties._updateProperty('sgm_data', tuple(sgm_groups))
+        logger.info('SGM properties set.')
 
 
-def createRules(room, event):
-    """
-    This event throw some specified events for room initial setup
-    """
+class CreateSharing(object):
+
+    def __init__(self, context, event):
+        """
+        Create the sharing localroles for already created groups.
+        """
+        self.context = context
+        room_id = self.context.getId()
+        pc = getToolByName(self.context, 'portal_catalog', None)
+        #set local roles of the room
+        self.setFolderLocalRoles(self.context,
+                                 list_groups=[{'id':'%s.members' % room_id,
+                                               'roles': ['Reader']},
+                                              {'id':'%s.membersAdv' % room_id,
+                                               'roles': ['Reader']},
+                                              {'id':'%s.hosts' % room_id,
+                                               'roles': ['Reader']},
+                                              {'id':'%s.coordinators' % room_id,
+                                               'roles': ['Reader']}])
+        areas = pc(path="/".join(self.context.getPhysicalPath()),
+                   object_provides=IRoomArea.__identifier__)
+        for area in areas:
+            groups = []
+            if area.portal_type == "NewsArea":
+                groups = [{'id': "%s.hosts" % room_id, 'roles': ['Reader']},
+                          {'id': '%s.members' % room_id, 'roles': ['Reader']},
+                          {'id': '%s.membersAdv' % room_id, 'roles': ['Contributor', 'Editor', 'EditorAdv']},
+                          {'id': '%s.coordinators' % room_id, 'roles': ['LocalManager', 'Contributor', 'Editor', 'EditorAdv', 'Reviewer']}]
+            elif area.portal_type == "DocumentsArea":
+                groups = [{'id': "%s.hosts" % room_id, 'roles': ['Reader']},
+                          {'id': '%s.members' % room_id, 'roles': ['Contributor', 'Editor']},
+                          {'id': '%s.membersAdv' % room_id, 'roles': ['Contributor', 'Editor', 'EditorAdv']},
+                          {'id': '%s.coordinators' % room_id, 'roles': ['LocalManager', 'Contributor', 'Editor', 'EditorAdv', 'Reviewer']}]
+            elif area.portal_type == "EventsArea":
+                groups = [{'id': "%s.hosts" % room_id, 'roles': ['Reader']},
+                          {'id': '%s.members' % room_id, 'roles': ['Contributor', 'Editor']},
+                          {'id': '%s.membersAdv' % room_id, 'roles': ['Contributor', 'Editor', 'EditorAdv']},
+                          {'id': '%s.coordinators' % room_id, 'roles': ['LocalManager', 'Contributor', 'Editor', 'EditorAdv', 'Reviewer']}]
+            elif area.portal_type == "PollsArea":
+                groups = [{'id': "%s.hosts" % room_id, 'roles': ['Reader']},
+                          {'id': '%s.members' % room_id, 'roles': ['Contributor', 'Editor']},
+                          {'id': '%s.membersAdv' % room_id, 'roles': ['Contributor', 'Editor', 'EditorAdv']},
+                         {'id': '%s.coordinators' % room_id, 'roles': ['LocalManager', 'Contributor', 'Editor', 'EditorAdv', 'Reviewer']}]
+            elif area.portal_type == "PloneboardForum":
+                groups = [{'id': "%s.hosts" % room_id, 'roles': ['Reader']},
+                          {'id': '%s.members' % room_id, 'roles': ['Contributor', 'Editor']},
+                          {'id': '%s.membersAdv' % room_id, 'roles': ['Contributor', 'Editor', 'EditorAdv', 'Reviewer']},
+                          {'id': '%s.coordinators' % room_id, 'roles': ['LocalManager', 'Contributor', 'Editor', 'EditorAdv', 'Reviewer']}]
+            elif area.portal_type == "Blog":
+                groups = [{'id': "%s.hosts" % room_id, 'roles': ['Reader']},
+                          {'id': '%s.members' % room_id, 'roles': ['Contributor', 'Editor']},
+                          {'id': '%s.membersAdv' % room_id, 'roles': ['Contributor', 'Editor', 'EditorAdv']},
+                          {'id': '%s.coordinators' % room_id, 'roles': ['LocalManager', 'Contributor', 'Editor', 'EditorAdv', 'Reviewer']}]
+            if groups:
+                self.setFolderLocalRoles(area.getObject(), groups)
+
+    def setFolderLocalRoles(self, folder, list_groups):
+        """
+        Set the local roles for a given folder
+        """
+        #block the inherit
+        folder.__ac_local_roles_block__ = True
+        #set the local roles
+        for group in list_groups:
+            folder.manage_addLocalRoles(group.get('id'),group.get('roles'))
+        #reindex the security
+        folder.reindexObjectSecurity()
 
 
 def createHomePage(room, event):
@@ -140,7 +254,7 @@ def createHomePage(room, event):
     """
 
 
-def createSharing(room, event):
+def CreateRules(room, event):
     """
     This event throw some specified events for room initial setup
     """
