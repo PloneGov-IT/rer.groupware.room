6993608cfd6ebab892f803a3ccc1c66c6284dedc
keul <luca@keul.it>
2013-05-13 16:05:33 +0000

    Supporto i18n anche per dominio Plone

--- rer/groupware/room/i18n.sh
+++ rer/groupware/room/i18n.sh
@@ -3,8 +3,11 @@
 DOMAIN='rer.groupware.room'
 
 i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --create ${DOMAIN} --merge locales/manual.pot .
-
 i18ndude sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
+
+# for the plone domain
+i18ndude rebuild-pot --pot i18n/plone.pot --create plone profiles/default/workflows
+i18ndude sync --pot i18n/plone.pot i18n/plone-??.po
 
 # Compile po files
 for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do
