672c8d0e2d028aaf175bf99d553152636d162bba
keul <luca@keul.it>
2013-05-15 14:42:58 +0000

    Reso più solido il caso in cui un gruppo gestito  da SGM non ci sia

--- rer/groupware/room/events/group_events.py
+++ rer/groupware/room/events/group_events.py
@@ -3,6 +3,7 @@
 from Products.CMFCore.utils import getToolByName
 from zope.app.component.hooks import getSite
 
+from rer.groupware.room import logger
 
 def removeNotifyGroup(info_dict, event):
     group_id = info_dict['group'].getId()
@@ -14,5 +15,9 @@ def removeNotifyGroup(info_dict, event):
     notification_groups = ["%s.notifyNewsEvents" % room_id, "%s.notifyDocs" % room_id]
     for group_id in notification_groups:
         group = acl_users.getGroup(group_id)
-        if info_dict['user_id'] in group.getMemberIds():
-            group.removeMember(info_dict['user_id'])
+        if group:
+            if info_dict['user_id'] in group.getMemberIds():
+                group.removeMember(info_dict['user_id'])
+                logger.info('Removed %s from %s' % (group_id, group.getId()))
+        else:
+            logger.warning("Can't find %s" % group_id)
