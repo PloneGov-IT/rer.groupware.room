28a46f84ca1c29ccf2c123b4a8c6c617b7f9ea7e
cekk <andrea.cecchi85@gmail.com>
2013-05-17 12:32:36 +0000

    fixed event

--- rer/groupware/room/events/default_room_events.py
+++ rer/groupware/room/events/default_room_events.py
@@ -255,7 +255,7 @@ class CreateSharing(object):
         #set local roles of the room
         self.setFolderLocalRoles(self.context,
                                  list_groups=[{'id':'%s.users' % room_id,
-                                               'roles': ['Active User']},
+                                               'roles': ['Active User', 'Reader']},
                                               {'id':'%s.hosts' % room_id,
                                                'roles': ['Reader']}],
                                  roles_block=True)
