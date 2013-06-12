a9972c7e1de7ad8bd3e83bec1c3bd35ed3b83829
cekk <andrea.cecchi85@gmail.com>
2013-06-12 07:24:07 +0000

    fixed viewlet

--- rer/groupware/room/browser/viewlets.py
+++ rer/groupware/room/browser/viewlets.py
@@ -2,26 +2,29 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 from rer.groupware.room.interfaces import IGroupRoom
 
+
 class GroupwareRoomViewletBase(ViewletBase):
     def __init__(self, context, request, view, manager):
         super(GroupwareRoomViewletBase, self).__init__(context, request, view, manager)
-        self.room=self.getRoomObj()
-    
+        self.room = self.getRoomObj()
+
     def render(self):
         if self.room:
             return self.index()
         else:
             return ""
-    
+
     def getRoomObj(self):
         for elem in self.context.aq_inner.aq_chain:
             if IGroupRoom.providedBy(elem):
                 return elem
         return None
-    
+
+
 class RERGroupwareRoomTitleViewlet(GroupwareRoomViewletBase):
     index = ViewPageTemplateFile('viewlets/groupware_room_title.pt')
-    
+
+
 class RERGroupwareRoomColorViewlet(GroupwareRoomViewletBase):
     """
     A Viewlet that allows to add some dynamic css in the  header
@@ -30,11 +33,10 @@ class RERGroupwareRoomColorViewlet(GroupwareRoomViewletBase):
         if not self.room:
             return ""
 #        color=self.room.getSubsiteColor()
-        image=self.room.getImage()
-        return_string=''
-        css = '#roomTitle {background-color:#552649'
+        image = self.room.getImage()
+        return_string = ''
+        # css = '#roomTitle {background-color:#552649'
         if image:
-            css='%s;background-image:url(%s); background-position: 100%% 0;' %(css,image.absolute_url())
-            css =css+'}'
-            return_string = "<style type='text/css'>%s</style>" %css
+            css = '#roomTitle {background-image:url(%s); background-position: 100%% 0;}' % image.absolute_url()
+            return_string = "<style type='text/css'>%s</style>" % css
         return return_string
