77ef5eb586b0b3a127f82ed343183bf552ba1b08
Giacomo Monari <arsenico13@hotmail.it>
2017-02-03 11:44:44 +0100

    [new] a user is removed from all the notifications group of a room only if it doesn't have any other role in that room

--- rer/groupware/room/events/userEvents.py
+++ rer/groupware/room/events/userEvents.py
@@ -17,6 +17,16 @@ def userRemovedFromGroup(event):
     active_groups = api.portal.get_registry_record('active_groups', interface=IRoomGroupsSettingsSchema)
     passive_groups = api.portal.get_registry_record('passive_groups', interface=IRoomGroupsSettingsSchema)
 
+    def get_roles():
+        """This function returns a list of active and passive roles.
+        """
+        roles = []
+        for agroup in active_groups:
+            roles.append(agroup.group_id)
+        for pgroup in passive_groups:
+            roles.append(pgroup.group_id)
+        return roles
+
     group_id = group._id            # training.hosts
     room = group_id.split('.')[0]   # training
     role = group_id.split('.')[1]   # hosts
@@ -25,17 +35,35 @@ def userRemovedFromGroup(event):
 
     for agroup in active_groups:
         if agroup.group_id == role:
-            userGroup = api.group.get_groups(username=user_id)
-            for elem in userGroup:
-                splitted_group = elem.id.split('.')
-                if splitted_group[-1] == 'notify' and splitted_group[0] == room:
-                    api.group.remove_user(groupname=elem.id, username=user_id)
+            userGroups = api.group.get_groups(username=user_id)
+            FOUND = False
+            roles = get_roles()
+            for group in userGroups:
+                splitted_group = group.id.split('.')
+                if splitted_group[0] == room:
+                    if splitted_group[-1] in roles:
+                        FOUND = True
+
+            if not FOUND:
+                for group in userGroups:
+                    splitted_group = group.id.split('.')
+                    if splitted_group[-1] == 'notify' and splitted_group[0] == room:
+                        api.group.remove_user(groupname=group.id, username=user_id)
 
 
     for pgroup in passive_groups:
         if pgroup.group_id == role:
-            userGroup = api.group.get_groups(username=user_id)
-            for elem in userGroup:
-                splitted_group = elem.id.split('.')
-                if splitted_group[-1] == 'notify' and splitted_group[0] == room:
-                    api.group.remove_user(groupname=elem.id, username=user_id)
+            userGroups = api.group.get_groups(username=user_id)
+            FOUND = False
+            roles = get_roles()
+            for group in userGroups:
+                splitted_group = group.id.split('.')
+                if splitted_group[0] == room:
+                    if splitted_group[-1] in roles:
+                        FOUND = True
+
+            if not FOUND:
+                for group in userGroups:
+                    splitted_group = group.id.split('.')
+                    if splitted_group[-1] == 'notify' and splitted_group[0] == room:
+                        api.group.remove_user(groupname=group.id, username=user_id)
